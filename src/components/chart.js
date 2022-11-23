7ae8c2e69681a39802321d3cb92a7bf11d273c0e
jbarribal <barribal.joshuageorge@ue.edu.ph>
2022-11-24 06:06:29 +0800

    update schema and chart

--- src/components/chart.js
+++ src/components/chart.js
@@ -5,7 +5,7 @@ import { data } from '../../database/data';
 const Chart = () => {
   return (
     <div>
-        <BarChart width={500} height={300} data={data}>
+        <BarChart width={450} height={300} data={data}>
             <Bar dataKey="uv" fill="#8884d8" />
         </BarChart>
     </div>
